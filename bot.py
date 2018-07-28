98dacb3a0eb57c7256e48cf11ed4395ff7d885ba
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-28 14:21:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix = "tb.")
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
     await ctx.send(':boot: Cya, {}, Ya loser!'.format(user.name))
-    await bot.kick(user)
+    await ctx.guild.kick(user)
 
 
 bot.run('NDcyNTY5Njk5MDQxODA0Mjk4.Dj1Smg.MbbM1cKvQYDRO0E8U50tb82mf1s')
