101c41d756a5bba5eb5dcd486ed1dc4fe2740d8a
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-27 21:39:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = commands.Bot(command_prefix = "tb.")
 @bot.command()
 async def botdown(ctx):
     if message.author.id == "369256915479560192":
-    await ctx.send('The bot will be down for expected maintenance')
-    await ctx.send('You can thank WolfBane for this.')
+     await ctx.send('The bot will be down for expected maintenance')
+     await ctx.send('You can thank WolfBane for this.')
     
     else:
-    await ctx.send(':no_entry: You can not run this command :no_entry:')
+     await ctx.send(':no_entry: You can not run this command :no_entry:')
 
 
 bot.run('NDcyNTY5Njk5MDQxODA0Mjk4.Dj1Smg.MbbM1cKvQYDRO0E8U50tb82mf1s')
