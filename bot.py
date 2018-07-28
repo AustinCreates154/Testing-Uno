9b4089fa2cdd23a29740731cfae52b185a3d8058
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-27 21:38:27 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = commands.Bot(command_prefix = "tb.")
 
 @bot.command()
 async def botdown(ctx):
-    if message.author.id == ("369256915479560192")
+    if message.author.id == "369256915479560192":
     await ctx.send('The bot will be down for expected maintenance')
     await ctx.send('You can thank WolfBane for this.')
     
