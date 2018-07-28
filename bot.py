43c6f4a932274cce793d97402bbe894aaa76b1c1
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-28 14:18:46 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,10 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "tb.")
 
-@bot.command()
-async def botdown(ctx):
-    if message.author.id == "369256915479560192":
-     await ctx.send('The bot will be down for expected maintenance')
-     await ctx.send('You can thank WolfBane for this.')
-    
-    else:
-     await ctx.send(':no_entry: You can not run this command :no_entry:')
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    await bot.say(':boot: Cya, {}, Ya loser!'.format(user.name))
+    await bot.kick(user)
 
 
 bot.run('NDcyNTY5Njk5MDQxODA0Mjk4.Dj1Smg.MbbM1cKvQYDRO0E8U50tb82mf1s')
