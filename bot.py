0a9f9a1b59d7230de86beae69c150a24e396d31e
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-28 14:48:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,14 @@ bot = commands.Bot(command_prefix = "tb.")
 async def kick(ctx, user: discord.Member):
     await ctx.send(':boot: Cya, {}, Ya loser!'.format(user.name))
     await ctx.guild.kick(user)
+    print('{} Has issued the kick command'.format(message.author))
+    
+@bot.command
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, user: discord.Member):
+    await ctx.send(':hammer: {} has been banned!!!'.format(user.name))
+    await ctx.guild.ban(user)
+    print('{} Has issues the kick command'.format (message.author))
 
 
 bot.run('NDcyNTY5Njk5MDQxODA0Mjk4.Dj1Smg.MbbM1cKvQYDRO0E8U50tb82mf1s')
