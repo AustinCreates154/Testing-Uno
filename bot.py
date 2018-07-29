5c4ca720130b46eba0a784bd718814315b85b4b8
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-07-29 08:29:21 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,6 +61,11 @@ async def botsgonnabedown(ctx):
     await ctx.send('The bot\'s gonna be down for a little bit sorry!')
     await ctx.send('You can thank WolfBane#5559 for the update!')
     
-
+@bot.command(pass_context=True)
+@commands.is_owner()
+async def echo(ctx, *args):
+    mesg = ' '.join(args)
+    await ctx.delete_message(ctx.message)    
+    return await ctx.send(mesg)
 
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
