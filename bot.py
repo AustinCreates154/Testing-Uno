ea49a2f1042330f95877b273420bc8e807b416d5
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 15:51:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,13 @@ bot = commands.Bot(command_prefix='vb!')
 @bot.command(pass_context=True)
 @commands.has_role('Admins')
 async def kick(ctx, user: discord.Member):
-    await ctx.send(':boot:' + discord.member + 'has been kicked by:{}!!'.format(ctx.message.author))
+    await ctx.send(':boot:{} has been kicked!!'.format(discord.Member))
     await ctx.kick(user)
-
+#Suggestion command below!
+@bot.command()
+async def suggestions(ctx):
+    await ctx.send(Here is my suggestions form!)
+    await ctx.send(https://docs.google.com/forms/d/e/1FAIpQLSe0R6BrOnXXWYhjLAr0IGHN7atdIy65OooH7fhRPLjyBm6tpw/viewform)
+    
 #Run's the bot
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
