e62229e18d6164f20c61102eb812a56584ab764d
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 16:11:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,15 @@ async def suggestions(ctx):
     await ctx.send('https://docs.google.com/forms/d/e/1FAIpQLSe0R6BrOnXXWYhjLAr0IGHN7atdIy65OooH7fhRPLjyBm6tpw/viewform')
 #Info command below!
 @bot.command(pass_context=True)
-async def info(ctx, user: discord.Member)
-    embed=discord.Embed(title='{}\'s info!'.format(discord.User
+async def info(ctx, user: discord.Member):
+    embed=discord.Embed(title='{}\'s info!'.format(discord.Member), description='Here\'s what I could find!')
+    embed.add_field(name='Username', value=user.name inline=True)
+    embed.add_field(name='User ID', value=user.id, inline=True)
+    embed.add_field(name='User Stauts', value=user.status, inline=True)
+    embed.add_field(name='User\'s Top Role', value=user.top_role, inline=True)
+    embed.add_field(name='User join server at', value=user.joined_at, inline=True)
+    
+    await ctx.send(embed=embed)
+    
 #Run's the bot
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
