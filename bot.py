152e014d280e1ce30e4799cf03c02ca2e779a94d
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 06:06:04 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ from discord.ext import commands
 #Make's the bot work
 bot = commands.Bot(command_prefix='vb!')
 #Makes the bot a bot
-await bot.change_status(game=discord.Game(name='I am a tad bit broken'))
+@bot.command()
+async def status(ctx)
+    await ctx.send('Thatnks for setting my status')
+    await bot.change_status(game=discord.Game(name='I am a tad bit broken'))
 
 #Run's the bot
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
