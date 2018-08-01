a6cc296b0ddf48a0e71940676a7796da018fe764
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 04:32:45 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix = "vb!")
 
 
 
-@bot.command()
+@bot.command(pass_context=False)
 async def help(ctx):
     embed = discord.Embed(title="Violet Brush Help Command", description="These are all of Violet Brush Bot\'s commands", color=0x00ff00)
     embed.add_field(name='Admin Commands', value='Kick, Ban')
@@ -43,7 +43,7 @@ async def info(ctx, user: discord.Member):
     
     await ctx.send(embed=embed)
     
-@bot.command()
+@bot.command(pass_context=False)
 async def botinfo(ctx):
     embed = discord.Embed(title="Bot's Info", description='This is all of my info!', color=0x00ff00)
     embed.add_field(name='Bot ID', value='472271106133458944')
