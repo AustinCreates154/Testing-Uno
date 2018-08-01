51d3a41d8036120430db90d130ae702a411bd762
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 06:02:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 #Make's the bot work
 bot = commands.Bot(command_prefix='vb!')
 #Makes the bot a bot
-
+bot.change_status(game=discord.Game(name='I am a tad bit broken'))
 
 #Run's the bot
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
