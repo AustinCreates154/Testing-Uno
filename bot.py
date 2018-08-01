037c0656a073e7901822ab1b1dcc7b2687c93f89
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-01 15:52:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ async def kick(ctx, user: discord.Member):
 #Suggestion command below!
 @bot.command()
 async def suggestions(ctx):
-    await ctx.send(Here is my suggestions form!)
-    await ctx.send(https://docs.google.com/forms/d/e/1FAIpQLSe0R6BrOnXXWYhjLAr0IGHN7atdIy65OooH7fhRPLjyBm6tpw/viewform)
+    await ctx.send('Here is my suggestions form!')
+    await ctx.send('https://docs.google.com/forms/d/e/1FAIpQLSe0R6BrOnXXWYhjLAr0IGHN7atdIy65OooH7fhRPLjyBm6tpw/viewform')
     
 #Run's the bot
 bot.run('NDcyMjcxMTA2MTMzNDU4OTQ0.Dj5hCA.ddJOnn9R_S2gJPNe-LiiMFurlzk')
