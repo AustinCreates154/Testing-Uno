b3225db657ae1ed8afa0fa97a0e40b1895d47726
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-02 20:41:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,15 @@ bot = commands.Bot(command_prefix='vb!')
 #Makes the bot a bot
 #Admin commands below!!
 @bot.command(pass_context=True)
-@commands.has_role('Admins')
-async def ban(ctx, user: discord.Member):
-    await ctx.send(':hammer: {} has been banned! :wave:'.format(user))
-    await ctx.guild.ban(user)
-@bot.command(pass_context=True)
-@commands.has_role('Admins')
-async def kick(ctx, user: discord.Member):
-    await ctx.send(':boot:{} has been kicked!'.format(user))
-    await ctx.guild.kick(user)
+#@commands.has_role('Admins')
+#async def ban(ctx, user: discord.Member):
+#    await ctx.send(':hammer: {} has been banned! :wave:'.format(user))
+#    await ctx.guild.ban(user)
+#@bot.command(pass_context=True)
+#@commands.has_role('Admins')
+#async def kick(ctx, user: discord.Member):
+#    await ctx.send(':boot:{} has been kicked!'.format(user))
+#    await ctx.guild.kick(user)
 #Suggestion command below!
 @bot.command()
 async def suggestions(ctx):
